bc3b95ec4614ef758dda69b5205ba8ed1e71b4c6
Jesús Gómez <jgomo3@gmail.com>
2017-04-24 23:56:41 -0400

    Add packaging info

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='ftport',
+    version='0.0.1',
+    description='Python ftplib extension for registering an alternative IP for'
+                ' the PORT command',
+    url='https://github.com/jgomo3/ftport',
+    author='Jesús Gómez',
+    author_email='jgomo3@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',  
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Internet :: File Transfer Protocol (FTP)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='ftp-client ftp',
+)
